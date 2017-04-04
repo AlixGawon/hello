b4c6d3b0802b4a4a216f30fbc72c79da1689cf33
Alix Gawon Lee <dlrkdnjs1211@gmail.com>
2017-04-04 16:08:34 +0900

    to eng

---
+++ src/IntDoubleCalculation.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Alix Gawon Lee on 2017-04-04.
+ */
+public class IntDoubleCalculation {
+    public static void main(String[]args){
+        int a = 2;
+        double b = 1.5;
+
+        System.out.println("int와 double의 사칙연산 결과 " + a*b );
+        //int와 double을 연산하면 저절로 double 자료형으로 변환된다.
+
+    }
+}
