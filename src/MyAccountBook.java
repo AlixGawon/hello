defb16088b537d146f7970da5de59f0c82145bf0
Alix Gawon Lee <dlrkdnjs1211@gmail.com>
2017-04-04 11:10:16 +0900

    initial commit

---
+++ src/MyAccountBook.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Alix Gawon Lee on 2017-04-04.
+ */
+public class MyAccountBook {
+    public static void main(String []args){
+        double transportationExpense = 1250.0;
+        double coffee = 1800.0;
+        double lunch = 5000.0;
+        double totalExpense = transportationExpense * 2 + coffee + lunch;
+
+        System.out.println("오늘 경비 = " + totalExpense);
+    }
+}
