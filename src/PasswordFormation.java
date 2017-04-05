1b5d9630005967c1c065da7ae9880fadbe24b886
Alix Gawon Lee <dlrkdnjs1211@gmail.com>
2017-04-05 10:45:23 +0900

    to eng

--- src/PasswordFormation.java
+++ src/PasswordFormation.java
@@ -17,7 +17,9 @@ public class PasswordFormation {
         //System.out.println(beingCapital);
 
 
-        String sum = inputted.substring(1,7);
+        String sum = inputted.substring(1);
+        //String sum = inputted.substring(1,7);
+        // 여기서 잘못 만들음. 끝이 어딘 줄 알고 7까지 지정함
 
 
         int length = inputted.length();
@@ -29,3 +31,27 @@ public class PasswordFormation {
     }
 
 }
+
+/* <강사님 모범답안>
+
+   Scanner in = new Scanner(System.in);
+
+   System.out.println("단어를 입력하세요!");
+
+   String inputted = in.nextLine();
+
+   String prefix = "$$";
+   String postfix = "##";
+   int squareLengthOfChar =
+         targetStr.length() * targetStr.length();
+
+         targetStr =
+             targetStr.substring(0,1).toUpperCase()
+             + targetStr.substring(1);//substring(1)은 두 번째부터 끝까지임.
+
+             string passwordTranslated =
+                  prefix + targetStr + squareLengthOfChar + postfix;
+
+             System.out.println(result);
+
+*/
