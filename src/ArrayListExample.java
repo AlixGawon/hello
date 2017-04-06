a919a4ecfa0a33a1fcdeb740ead9e6a0749b07f2
Alix Gawon Lee <dlrkdnjs1211@gmail.com>
2017-04-06 12:58:26 +0900

    to eng

---
+++ src/ArrayListExample.java
@@ -0,0 +1,51 @@
+/**
+ * Created by Alix Gawon Lee on 2017-04-06.
+ */
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[]args) {
+
+        String inform = "오늘의 2번째 포스팅역시 맛집!" +
+                "주말에 남자친구랑 너무 맛있게 먹고온 구로 초밥뷔페를 추천해드리려고하는데요!!" +
+                "구로 초밥뷔페집의 이름은 스시메이진이라는 곳이에요^^♥" ;
+
+        //1. inform 스트링을 리스트로 변환
+        // 공백을 기준으로 분리( 공백을 딜리미터로 분리)
+
+        List<String> myList = new ArrayList<String>(Arrays.asList(inform.split(" "))); //ArrayList는 초깃값 필요 없음 그리고 split은 무엇을 기준으로 나누는 메소드, asㅣist는 myList에 inform.split을 넣을 때
+        for (String e : myList) {
+
+            System.out.println(e);
+        }
+        System.out.println(myList);
+
+
+        //2. 리스트를 순회해서 전체 단어의 글자수의 평균을 구한다.
+
+        int myListAmount = 0;
+        int sum = 0;
+        int sumAverage = 0;
+        List<String> newMyList = new ArrayList<String>(); //새로운 리스트 생성한것임
+        for (String e : myList) {
+            myListAmount = e.length();
+            sum += myListAmount;
+            //System.out.println(sum);
+            //3. 단어가 5자 이하인 새로운 리스트를 생성한다.
+            if (e.length() <5){
+                newMyList.add(e);
+            }
+        }
+        sumAverage = sum / myListAmount;
+        System.out.println(sumAverage);
+
+
+
+
+
+
+    }
+}
---
+++ src/StringArrayExampleArrayListVersion.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Alix Gawon Lee on 2017-04-06.
+ */
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class StringArrayExampleArrayListVersion {
+    public static void main(String[]args){
+
+        String[] strArr = {
+                "숟가락반상 마실", "스시메이진", "맛찬들", "최우영스시", "철판목장", "용호낙지", "은행골", "메이비"
+                , "돌배기집", "구로곱창", "두끼 구로디지털단지점", "아비꼬", "마루가메제면", "킹콩부대찌개 구로디지털점"
+                , "바른식탁", "말뚝곱창", "보나베띠", "춘자싸롱", "황제해물보쌈", "호우 양꼬치"};
+
+        /*List<String> goument = new ArrayList<>();
+        goumet.add("숟가락반상 마실");
+        goumet.add("스시메이진");
+         */
+        List<String> goumet = Arrays.asList(
+                "숟가락반상 마실", "스시메이진", "맛찬들", "최우영스시", "철판목장", "용호낙지", "은행골", "메이비"
+                , "돌배기집", "구로곱창", "두끼 구로디지털단지점", "아비꼬", "마루가메제면", "킹콩부대찌개 구로디지털점"
+                , "바른식탁", "말뚝곱창", "보나베띠", "춘자싸롱", "황제해물보쌈", "호우 양꼬치"
+
+        );
+        List<String> goumetUnderThreeChar = new ArrayList<>();
+
+        for (String e : goumet) {
+            if (e.length() <= 3) {
+                goumetUnderThreeChar.add(e);
+            }
+        }
+// 새로 만들어진 리스트 출력
+        for (String e : goumetUnderThreeChar) {
+            System.out.println(e);
+        }
+    }
+}
